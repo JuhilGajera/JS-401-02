f1309c232fb0fc72ff9273c89dc2fcac13207654
JuhilGajera <juhilgajera2@gmail.com>
2023-01-05 22:51:27 +0530

    first

---
+++ index.js
@@ -0,0 +1,51 @@
+function myFunction1() {
+    document.getElementById("1").style.backgroundColor = "blue";
+}
+function myFunction2() {
+    document.getElementById("2").style.backgroundColor = "#32fc2b";
+}
+// function myFunction3() {
+//     document.getElementById("3").style.backgroundColor = "lightblue";
+// }
+function myFunction4() {
+    document.getElementById("4").style.backgroundColor = "#32fc2b";
+}
+// function myFunction5() {
+//     document.getElementById("5").style.backgroundColor = "lightblue";
+// }
+function myFunction6() {
+    document.getElementById("6").style.backgroundColor = "#32fc2b";
+}
+// function myFunction7() {
+//     document.getElementById("7").style.backgroundColor = "lightblue";
+// }
+function myFunction8() {
+    document.getElementById("8").style.backgroundColor = "#32fc2b";
+}
+function myFunction9() {
+    document.getElementById("9").style.backgroundColor = "green";
+}
+function myFunction10() {
+    document.getElementById("10").style.backgroundColor = "#32fc2b";
+}
+// function myFunction11() {
+//     document.getElementById("11").style.backgroundColor = "lightblue";
+// }
+function myFunction12() {
+    document.getElementById("12").style.backgroundColor = "#32fc2b";
+}
+// function myFunction13() {
+//     document.getElementById("13").style.backgroundColor = "lightblue";
+// }
+function myFunction14() {
+    document.getElementById("14").style.backgroundColor = "#32fc2b";
+}
+// function myFunction15() {
+//     document.getElementById("15").style.backgroundColor = "lightblue";
+// }
+function myFunction16() {
+    document.getElementById("16").style.backgroundColor = "#32fc2b";
+}
+function myFunction17() {
+    document.getElementById("17").style.backgroundColor = "#f54e80";
+}
